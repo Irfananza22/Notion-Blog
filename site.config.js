82dc26f8a412eaf674d1236b2c6bd8afce784d11
Irfan Nur Afrillianza <130213366+Irfananza22@users.noreply.github.com>
2023-11-09 19:52:15 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,29 +1,29 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "Reza",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "developer",
+    name: "Irfan Anza",
+    image: "https://notion-avatar.vercel.app/api/svg/eyJmYWNlIjo5LCJub3NlIjo2LCJtb3V0aCI6MywiZXllcyI6MTAsImV5ZWJyb3dzIjowLCJnbGFzc2VzIjoyLCJoYWlyIjoyLCJhY2Nlc3NvcmllcyI6MCwiZGV0YWlscyI6MCwiYmVhcmQiOjAsImZsaXAiOjEsImNvbG9yIjoiI2ZhZGIxNCIsInNoYXBlIjoiY2lyY2xlIn0=", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "dev",
     bio: "Iam student at Gunadarma.",
-    email: "m.rezahidayat.rh@gmail.com",
-    linkedin: "rezahidayatrh",
-    github: "rezapace",
-    instagram: "rezarh.go",
+    email: "irfananza22@gmail.com",
+    linkedin: "irfananza",
+    github: "irfananza22",
+    instagram: "irfan_afrillianza",
   },
   projects: [
     {
-      name: `Reza`,
-      href: "https://github.com/rezapace",
+      name: `Irfan Anza`,
+      href: "https://github.com/Irfananza22",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "Kumal",
-    description: "welcome to Kumal-Blog!",
+    title: "Irfan's Blog",
+    description: "welcome to Irfan's-Blog!",
   },
 
   // CONFIG configration (required)
-  link: "https://kumal.notion.site/0f80822f90c9466386c3434b96ac3827?v=6f2affab5d8b4db381e06a2dd578429b&pvs=4",
+  link: "https://fan-jade-845.notion.site/a68646c139574114be1d2578188331c0?v=5562de4c66e74b188bff4c5c3f80f494&pvs=4",
   since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
